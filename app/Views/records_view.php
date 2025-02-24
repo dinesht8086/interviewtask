ce9b63d5dad9daf3bac3cd0e27954835921adb69
dinesht8086 <dinesht8086@gmail.com>
2025-02-24 09:17:52 +0530

    modified some changesin files

--- app/Views/records_view.php
+++ app/Views/records_view.php
@@ -32,7 +32,7 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <form action="<?= base_url('/records/create'); ?>" method="post"  enctype="multipart/form-data"> 
+                            <form action="<?= base_url('/records/create'); ?>" method="post"  enctype="multipart/form-data">  <?= csrf_field() ?> 
                                 <div class="mb-3">
                                     <label for="name" class="form-label">Name</label>
                                     <input type="text" class="form-control" id="name" name="name" placeholder="Enter name" required>
